855a7856e2c28ce17aeb50bfe8264598b2147b7c
Anthony Key <anthony@mesmerisevr.com>
2023-06-19 23:16:21 +0100

    feat: remove contact instant

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -108,12 +108,18 @@ export default function Contact(props: {}) {
           <h2 className="text-gray-400 pb-3">
             If you have any enquiries, feel free to get in touch.
           </h2>
-          <div className="h-full flex flex-col justify-center text-white">
-            <div className="relative mb-6">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+
+          <p className="text-gray-100 font-semibold">{progress}</p>
+
+          <div className="p-4 dark:bg-[#2d3656] rounded mt-5 flex flex-col border-black border">
+            <h2 className=" text-lg font-semibold text-gray-400">
+              Contact Information
+            </h2>
+            <a href="mailto:anthonykey123@hotmail.co.uk">
+              <span className="flex-inline flex pl-3 items-center">
                 <svg
                   aria-hidden="true"
-                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  className="w-5 h-5 text-gray-400 dark:text-gray-400"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -121,20 +127,14 @@ export default function Contact(props: {}) {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                 </svg>
-              </div>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                className="rounded-full bg-[#575dc2] border border-gray-300 text-white text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-[#2d3656] dark:border-gray-600 dark:placeholder-gray-400 dark:text-whte dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Email"
-              />
-            </div>
+                <p className="ml-2 text-gray-400">
+                  anthonykey123@hotmail.co.uk
+                </p>
+              </span>
+            </a>
 
-            <div className="relative mb-6">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <a href="mailto:anthonykey123@hotmail.co.uk">
+              <span className="flex-inline flex pl-3 items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -143,76 +143,10 @@ export default function Contact(props: {}) {
                   className="w-5 h-5 text-gray-400"
                 >
                   <path d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                </svg>
-              </div>
-              <input
-                type="text"
-                value={phone}
-                onChange={(e) => {
-                  setPhone(e.target.value);
-                }}
-                className="bg-gray-50 border border-gray-300 text-white text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-[#2d3656] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Phone"
-              />
-            </div>
-            <textarea
-              id="large-input"
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-              className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg dark:bg-[#2d3656] sm:text-md resize-vertical h-auto focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Ask me anything"
-            ></textarea>
-            <button
-              type="button"
-              onClick={() => {
-                makePostRequest(url, payload);
-              }}
-              className="border border-white mt-6 font-semibold text-white  hover:bg-white hover:text-[#1c223b] rounded-full px-5 py-2.5 text-center inline-flex items-center justify-center  mr-2 mb-2"
-            >
-              Submit
-            </button>
-
-            <p className="text-gray-100 font-semibold">{progress}</p>
-
-            <div className="p-4 dark:bg-[#2d3656] rounded mt-5 flex flex-col border-black border">
-              <h2 className=" text-lg font-semibold text-gray-400">
-                Contact Information
-              </h2>
-              <a href="mailto:anthonykey123@hotmail.co.uk">
-                <span className="flex-inline flex pl-3 items-center">
-                  <svg
-                    aria-hidden="true"
-                    className="w-5 h-5 text-gray-400 dark:text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                  </svg>
-                  <p className="ml-2 text-gray-400">
-                    anthonykey123@hotmail.co.uk
-                  </p>
-                </span>
-              </a>
-
-              <a href="mailto:anthonykey123@hotmail.co.uk">
-                <span className="flex-inline flex pl-3 items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-gray-400"
-                  >
-                    <path d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                  </svg>{" "}
-                  <p className="ml-2 text-gray-400">07904684550</p>
-                </span>
-              </a>
-            </div>
+                </svg>{" "}
+                <p className="ml-2 text-gray-400">07904684550</p>
+              </span>
+            </a>
           </div>
         </div>
       </div>
