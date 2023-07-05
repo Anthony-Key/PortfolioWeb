8a500a717f834d19b972fd94dfd5719db91060bf
Anthony Key <anthony@mesmerisevr.com>
2023-07-05 10:23:10 +0100

    chore: changed wording

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -64,7 +64,7 @@ export default function Hero() {
                 d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
               />
             </svg>
-            Looking for Permanent / Contract for a Unity Developer
+            Looking for a Permanent / Contract Unity Developer Role
           </h3>
           <button
             onClick={() => {
